e55e49fc48501ec99bcfd661c64f54f2c5235955
Edullidan <128309694+Edullidan@users.noreply.github.com>
2023-10-16 00:49:09 +0400

    comment

---
+++ src/pages/components/CreatePost/create-post.js
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from "react";
+import styles from "./create-post.module.css";
+import { FaRegPlusSquare } from "react-icons/fa";
+import PostList from "../PostList/PostList";
+
+
+function Posts() {
+  const [postData, setPostData] = useState({
+    text: "",
+    image: null,
+  });
+  const [posts, setPosts] = useState([]);
+  const [isFormOpen, setIsFormOpen] = useState(false);
+
+  
+
+  const handleImageChange = (e) => {
+    const imageFile = e.target.files[0];
+    setPostData({ ...postData, image: imageFile });
+  };
+
+  async function createPost(formData) {
+    const response = await fetch("/api/post", {
+      method: "POST",
+      body: formData,
+    });
+
+    return response;
+  }
+
+  const handleCreatePost = async () => {
+    try {
+      const formData = new FormData();
+      formData.append("text", postData.text);
+      formData.append("image", postData.image);
+
+      const response = await createPost(formData);
+
+      if (response.status === 200) {
+        setPosts([...posts, postData]);
+        setPostData({ text: "", image: null });
+        setIsFormOpen(false);
+      } else {
+        console.error("Error creating post. Status code:", response.status);
+      }
+    } catch (error) {
+      console.error("Error creating post:", error.message);
+    }
+  };
+  const handleLikePost = (index) => {
+    const updatedPosts = [...posts];
+    updatedPosts[index].likes++; 
+    setPosts(updatedPosts);
+  };
+
+  return (
+    <div className={styles.container}>
+      <div onClick={() => setIsFormOpen(true)} className={styles.addIcon}>
+        <FaRegPlusSquare className={styles.icon} />
+        <span>Create</span>
+        <div className={styles.createPostForm}>
+          <div className={styles.createPostFormInner}></div>
+          {isFormOpen ? (
+            <>
+              <input
+                type="file"
+                name="image"
+                accept="image/*"
+                onChange={handleImageChange}
+              />
+              <button onClick={handleCreatePost} className={styles.post__button}>
+                Create Post
+              </button>
+            </>
+          ) : null}
+        </div>
+      </div>
+      <div className={styles.postListContainer}>
+        <PostList posts={posts} handleLikePost={handleLikePost} />
+      </div>
+    </div>
+  );
+}
+
+export default Posts;
---
+++ src/pages/components/Sign/Sign.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import styles from "./Sign.module.css";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+
+function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response.status === 200) {
+        console.log("Login successful");
+        router.push("/Home");
+      } else {
+        console.error("Login error");
+      }
+    } catch (error) {
+      console.error("Error sending login request", error);
+    }
+  };
+
+  return (
+    <div className={styles.sign_in_container}>
+        <img
+        className={styles.picture}
+        src='https://www.thehoth.com/wp-content/uploads/2020/08/Instagram-768x1088.png'
+        alt='picture'
+      />
+      <form className={styles.sign_in_form} onSubmit={handleSubmit}>
+        <input
+          className={styles.email}
+          type='email'
+          placeholder='Phone, username or email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          className={styles.password}
+          type='password'
+          placeholder='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input className={styles.login} type='submit' value='Login' />
+      </form>
+      <img
+        className={styles.logo}
+        src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/840px-Instagram_logo.svg.png'
+        alt='Logo'
+      />
+      <div className={styles.create_account}>
+        <p>Don't have an account yet?</p>
+        <Link href={"/signup"}>
+          <button className={styles.create_account_button}>
+            Create an account
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default SignIn;
